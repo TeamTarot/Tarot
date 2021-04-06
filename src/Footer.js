9f592b2b21efab4583f43a9467e652b4fb4d3e5d
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-04-05 17:52:01 -0700

    update footer copy

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>&copy; Best Books</Navbar.Brand>
+        <Navbar.Brand>&copy; Tarot</Navbar.Brand>
       </Navbar>
     );
   }
