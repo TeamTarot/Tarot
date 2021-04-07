78471e23d24a6210a18251bd774939ad83e76f7c
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-04-07 15:06:29 -0700

    ewr

---
+++ src/Card.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import { Tooltip } from 'react-bootstrap';
+import './assets/TempHang.jpg';
+//Photo by Lucas Pezeta from Pexels
+class Cardd extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          draw:[],
+          showDeck: false,
+        };
+      }
+//add a reverse function? Maybe a 3 to 1 odds of upward facing vs reverse card draws
+
+     readMore = (props) => {
+        return (<Tooltip id="read-more" {...props}>
+            {props.desc}
+        </Tooltip>
+)
+}
+//add another effect to the hover where the tarot image fades a little as the information gets displayed on hover
+    futureRender = (card) =>{
+
+        return (
+            <OverlayTrigger
+    placement="right"
+    delay={{ show: 250, hide: 400 }}
+    overlay={this.readMore(card)}
+  >
+    <Card className="tarot-card">
+      {/* <Card.Img variant="top" src={require('./assets/TempHang.jpg')} /> */}
+      <Card.ImgOverlay>
+
+    <Card.Title>{card.name}</Card.Title>
+    <Card.Body>
+    <Card.Text>
+      {card.meaning_up}
+    </Card.Text>
+
+    </Card.Body>
+
+    </Card.ImgOverlay>
+        </Card>
+        </OverlayTrigger>
+        )
+
+    }
+
+render(){
+    return this.futureRender(this.props.use)
+}
+
+}
+
+export default Cardd;   
+
+// <Card className="card-1">
+//     <Card.Header>
+//       <Nav variant="tabs" defaultActiveKey="#first">
+//       <Nav.Item>
+//         <Nav.Link href="#first">Basic Meaning</Nav.Link>
+//       </Nav.Item>
+//       <Nav.Item>
+//         <Nav.Link href="#link">Arcana Info</Nav.Link>
+//       </Nav.Item> 
+//       </Nav> 
+//       </Card.Header>
+//       <Card.Img variant="top" src="holder.js/100px180" />
+//       <Card.ImgOverlay>
+
+
+//     <Card.Title>{use.name}</Card.Title>
+
+//     <Card.Text>
+//       {use.meaning_up}
+//     </Card.Text>
+//     <Card.Text>
+//       {use.desc}
+//     </Card.Text>
+ 
+
+//     </Card.ImgOverlay>
+//         </Card>
--- src/CardTable.js
+++ src/CardTable.js
@@ -8,12 +8,24 @@ class CardTable extends React.Component {
     super(props);
     this.state = {
       user: this.props.userObj,
+<<<<<<< Updated upstream
       draw:[]
      
       
+=======
+      draw:[],
+      journal:'',
+      showDeck: false,
+      today:{
+        cardSet:[],
+        date: new Date(),
+        journal:'',
+    }
+>>>>>>> Stashed changes
     };
     // const user = this.props.auth0;
   }
+<<<<<<< Updated upstream
 
 
 
@@ -34,6 +46,35 @@ class CardTable extends React.Component {
       const hand = axios.get("http://localhost:3001/draw", {});
       this.setState({draw: hand})
       
+=======
+  async componentDidMount() {
+   
+    try{
+      await axios.get("http://localhost:3001/user", {params:{email: this.props.auth0.user.email}})
+      .then(result => {
+        this.props.useHandle(result)})
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+ saveReading = async (e) =>{
+   e.preventDefault();
+   await axios.post(`http://localhost:3001/reading`, {params: {email:this.props.auth0.user.email, reading: [...this.state.today]}} )
+   console.log("in save" , this.state) 
+ }
+
+
+
+  handleDraw = (e) => {
+    try{
+      e.preventDefault();
+      const hand = await axios.get("http://localhost:3001/draw", {})
+      this.setState({draw: hand,
+                     showDeck: true,
+                     today:{date: this.state.today.date, cardSet:[...hand], journal: this.state.today.journal} })
+      console.log("handle draw hand:", hand)
+>>>>>>> Stashed changes
     }catch(err){
         console.log(err)
     }
@@ -46,6 +87,7 @@ class CardTable extends React.Component {
       <>
 {       this.state.draw.length > 0
       ?<>
+<<<<<<< Updated upstream
       <div className='card-box'>
 
           <div className="card-1">
@@ -68,6 +110,24 @@ class CardTable extends React.Component {
       </>
       :<button onClick={(e)=>{this.handleDraw()}}>Click me for fortune</button>
 }
+=======
+       
+       {/* <CardDeck >
+          {this.props.userObj.cards.map((card) =>{
+      return <Cardd use={card} id={card._id}/>})}
+       </CardDeck> */}
+
+       <div>{this.state.draw}</div>
+       {this.state.draw.length && <div>this.state.draw</div>}
+       <div className="reflections">
+       <textarea placeholder="Journal your thoughts and reflections" style={{height:"150px"}} onChange={(e)=> this.setState({
+                                                                    today:{date: this.state.today.date, cardSet: this.state.today.cardSet,  journal: e.target.value}})}/>
+       <button onClick={(e)=> {this.saveReading(e)}}>Click to save</button>
+       </div>
+       </>
+      :<div className="draw-button"><button onClick={(e)=>{this.handleDraw(e)}}>Click me for fortune</button></div>
+    }
+>>>>>>> Stashed changes
       </>
     );
   }
