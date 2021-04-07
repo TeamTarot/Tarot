d53cebbefcd66d0bffd7ec4ca9458c583d531edd
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-06 21:50:53 -0700

    got react accordian completed for journal entries based on user obj and started the delete function for removing a journal entry.. going to be testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 import Header from './Header';
 import Login from './Login';
 import Profile from './Profile';
@@ -18,17 +19,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user:{}
+      user: {}
     };
-   
+
   }
 
 
   handleUser = (upd) => {
-  this.setState({
-    user:upd
-  })
-}
+    this.setState({
+      user: upd
+    })
+  }
+
+  handleDeleteReading = async (id) => {
+    const SERVER = 'http://localhost:3001';
+    const deletedJournal = await axios.delete(`${SERVER}/reading/${id}`, { params: { id: this.user.data.id } });
+    console.log('after delete success', deletedJournal);
+
+    const newUserObj = this.state.user.filter((journal, iD) => {
+      return id !== iD;
+    });
+    this.setState({ user: newUserObj });
+    // console.log('newUserObj in delete', newUserObj);
+  }
+
   render() {
     console.log('app', this.props);
     console.log(this.props.auth0);
@@ -47,7 +61,7 @@ class App extends React.Component {
             </Route>
             <Route exact path="/profile">
               {this.props.auth0.isAuthenticated ?
-                <Profile userObj={this.state.user} /> : <Redirect to='/' />
+                <Profile userObj={this.state.user} handleDeleteReading={this.handleDeleteReading}/> : <Redirect to='/' />
               }
             </Route>
             {/* <Route exact path="/about">
--- src/Profile.js
+++ src/Profile.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-import { Container, Card, Image } from 'react-bootstrap';
+import { Container, Card, Image, Accordion, Button, Form } from 'react-bootstrap';
 
 class Profile extends Component {
+
+
   render() {
-    console.log('getting userobj from App?', this.props.userObj);
+    console.log('getting userobj from App?', this.props.userObj.data.cards[0]);
+    const cardData = this.props.userObj.data.cards;
     const { user } = this.props.auth0;
     return (
       <>
         <Container className="mt-5 mb-5">
-          <Image style={{width: '16rem'}} src={user.picture} roundedCircle />
+          <Image style={{ width: '16rem' }} src={user.picture} roundedCircle />
           <Card.Body>
             <Card.Title >🔮 Hello {user.name} 🔮</Card.Title>
           </Card.Body>
         </Container>
 
-        
+        <Container>
+          {cardData.map((entry, index) => (
+            <Accordion key={index}>
+              <Card>
+                <Card.Header>
+                  <Card.Title>{entry.date}</Card.Title>
+                  <Accordion.Toggle as={Button} variant="dark" eventKey="0">
+                    View
+              </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <Form>
+
+                      <Form.Group>
+                        <Form.Control as="textarea" rows={4} placeholder={entry.journal} name="journal" />
+                      </Form.Group>
+
+                      <Button variant="danger" onClick={() => this.handleDeleteReading(index)}>delete</Button>
+                      <Button variant="info" onClick={() => this.displayUpdateModal(index)}>update</Button>
+                    </Form>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          ))}
+        </Container>
       </>
     );
   }
